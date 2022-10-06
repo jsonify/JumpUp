a62498ee66a566c9a42d367b07201e3dd256bd9e
Jason Rueckert <phlacin@gmail.com>
2022-10-06 10:16:08 -0700

    got prototype started

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -1,26 +1,26 @@
 extends KinematicBody2D
 
 var velocity := Vector2.ZERO
-const SPEED := 300
 const GRAVITY := 30
-const JUMPFORCE := -900
+const JUMPFORCE := -700
+
+var horizontal_speed := 200.0
+var horizontal_direction := 1
+
 
 func _physics_process(delta: float) -> void:
-	print(position.x)
-		
-	if Input.is_action_pressed("right"):
-		velocity.x = SPEED
+	if horizontal_direction == -1:
 		$Sprite.flip_h = true
-		
-	if Input.is_action_pressed("left"):
-		velocity.x = -SPEED
-		$Sprite.flip_h = false
-		
-	velocity.y += GRAVITY
 	
-	if Input.is_action_pressed("jump") and is_on_floor():
+	if is_on_wall():
+		horizontal_direction *= -1
+		$Sprite.flip_h = !$Sprite.flip_h
+		
+	velocity.x = horizontal_speed * horizontal_direction
+	
+	velocity.y += GRAVITY
+#
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMPFORCE
 		
 	velocity = move_and_slide(velocity, Vector2.UP)
-
-	velocity.x = lerp(velocity.x, 0, 0.2)
