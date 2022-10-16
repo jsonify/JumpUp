be9e97c50680bfd0cf106b67541f850815e315fd
Jason Rueckert <phlacin@gmail.com>
2022-10-16 00:10:22 -0700

    Added second checkpoint, trapdoor closure

---
+++ Scenes/Platforms/Trapdoor.gd
@@ -0,0 +1,21 @@
+extends StaticBody2D
+
+onready var animation_player := $AnimationPlayer
+onready var coll_shape := $CollisionShape2D
+var trapdoor_triggered := false
+
+
+func _ready() -> void:
+	pass
+	
+func _process(delta: float) -> void:
+	pass
+			
+
+func handle_checkpoint_reached():
+	pass
+		
+
+func trapdoor1_close():
+	pass
+
--- Scenes/Platforms/Trapdoor2.gd
+++ Scenes/Platforms/Trapdoor2.gd
@@ -1,11 +1,10 @@
 extends "Trapdoor.gd"
 
-
 func _ready() -> void:
-#	this will get the Checkpoint1 node
-	var checkpoint = get_tree().get_root().find_node("Checkpoint1", true, false)
+#	this will get the checkpoint2 node
+	var checkpoint = get_tree().get_root().find_node("Checkpoint2", true, false)
 #	this will connect to the signal in the checkpoint
-	checkpoint.connect("checkpoint1_reached", self, "handle_checkpoint_reached")
+	checkpoint.connect("checkpoint2_reached", self, "handle_checkpoint_reached")
 	animation_player.play("open")
 	
 func _process(delta: float) -> void:
@@ -24,4 +23,3 @@ func handle_checkpoint_reached():
 
 func trapdoor1_close():
 	coll_shape.set_deferred("disabled", false)
-
