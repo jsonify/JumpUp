6ea4ec669fb3a459ea2437bbe0c1d8e412a3bdae
Jason <phlacin@gmail.com>
2022-10-15 22:34:55 -0700

    got tiling work done

--- Scenes/Platforms/Trapdoor1.gd
+++ Scenes/Platforms/Trapdoor1.gd
@@ -1,7 +1,9 @@
 extends StaticBody2D
 
 onready var animated_sprite := $AnimatedSprite
+onready var animation_player := $AnimationPlayer
 onready var coll_shape := $CollisionShape2D
+var trapdoor_triggered := false
 
 
 func _ready() -> void:
@@ -9,11 +11,23 @@ func _ready() -> void:
 	var checkpoint = get_tree().get_root().find_node("Checkpoint1", true, false)
 #	this will connect to the signal in the checkpoint
 	checkpoint.connect("checkpoint1_reached", self, "handle_checkpoint_reached")
+	animated_sprite.visible = false
 	
+func _process(delta: float) -> void:
+		if trapdoor_triggered:
+			animation_player.play("closing")
+			yield(get_tree().create_timer(0.5),"timeout")
+			animation_player.play("closed")
+			
+
 func handle_checkpoint_reached():
-	trapdoor1_close()
+	if trapdoor_triggered == false:
+		trapdoor1_close()
+		trapdoor_triggered = true
+		
 	
 
 func trapdoor1_close():
 	coll_shape.set_deferred("disabled", false)
+#	animated_sprite.visible = true
 
